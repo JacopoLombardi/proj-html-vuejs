19ddc9c2c348539894660754e86bfb0d541f0511
JacopoLombardi <lombajacopo@gmail.com>
2024-04-09 01:09:43 +0200

    [fix] made the data dynamic into Main_jumbotron

---
+++ public/jsFolder/main.js
@@ -0,0 +1,78 @@
+
+
+//////////////////  jumbotron  //////////////////////
+
+
+// array del titolo e testo
+export const titleAndTextArray = [
+   {
+      title: 'start investing in you',
+      text: 'With over 1200 courses in 18 subjects, you\'re guaranteed to find something that\'s right for you.',
+      textBtn: 'join for free'
+   }
+];
+
+
+
+
+// array di oggetti della composizione dell'immagine
+export const imageCompositionArray = [
+   {
+      class: 'img_base',
+      src: '../../../public/img/base.png',
+      alt: 'immagine base'
+   },
+   {
+      class: 'img_book1',
+      src: '../../../public/img/book1.png',
+      alt: 'immagine libro 1'
+   },
+   {
+      class: 'img_book2',
+      src: '../../../public/img/book2.png',
+      alt: 'immagine libro 2'
+   },
+   {
+      class: 'img_lens',
+      src: '../../../public/img/magnifier.png',
+      alt: 'immagine lente d\'ingrandimento'
+   },
+   {
+      class: 'img_bubblespeech',
+      src: '../../../public/img/bubblespeech.png',
+      alt: 'immagine discorso in bolla'
+   }
+];
+
+
+
+
+// array di oggetti delle cards
+export const cardsArray = [
+   {
+      class: 'fa-globe',
+      title: 'Worldwide Recognize',
+      text: 'Our online course certificates can be used around the world and also in most popular universities and companies'
+   },
+   {
+      class: 'fa-desktop',
+      title: 'Mostly Online Learning (sd)',
+      text: 'Masterstudy online certificates can be obtained in a range of specialized areas and typically take about a year to complete'
+   },
+   {
+      class: 'fa-calendar-days',
+      title: 'Graduate in as little as 1 Year',
+      text: '>Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy'
+   }
+];
+
+
+
+
+
+
+
+
+
+
+
--- src/components/partials/Main_jumbotron.vue
+++ src/components/partials/Main_jumbotron.vue
@@ -5,10 +5,18 @@
 
 import {store} from '../.././data/store'
 
+// importo gli oggetti dal file JS dedicato
+import {titleAndTextArray, imageCompositionArray, cardsArray} from '../../../public/jsFolder/main'
+
+
 export default {
+
   data(){
     return{
-      store
+      store,
+      titleAndTextArray,
+      imageCompositionArray,
+      cardsArray
     };
   },
 
@@ -27,41 +35,44 @@ export default {
 
       <div class="_container_custom_sm  _jumbotron  d-flex  align-items-center">
          <!-- titolo e testo -->
-         <div class="w-50">
-         <h1 class="capitalized_  fw-normal">start investing in you</h1>
-         <p class=" fw-semibold  fs-5  my-4">With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you.</p>
-         <button class="btn  btn-primary  uppercase_  fw-semibold  rounded-5  py-3  px-4">join for free</button>
-         </div>
+        <div 
+          v-for="item in titleAndTextArray"
+          :key="item.id"
+          class="w-50"
+        >
+          <h1 class="capitalized_  fw-normal">{{ item.title }}</h1>
+          <p class=" fw-semibold  fs-5  my-4">{{ item.text }}</p>
+          <button class="btn  btn-primary  uppercase_  fw-semibold  rounded-5  py-3  px-4">{{ item.textBtn }}</button>
+        </div>
 
          <!-- composizione dell'immagine -->
-         <div class="position-relative  ms-5">
-         <img class="img_base" src="../../../public/img/base.png" alt="">
-         <img class="img_book1  position-absolute" src="../../../public/img/book1.png" alt="">
-         <img class="img_book2  position-absolute" src="../../../public/img/book2.png" alt="">
-         <img class="img_lens  position-absolute" src="../../../public/img/magnifier.png" alt="">
-         <img class="img_bubblespeech  position-absolute" src="../../../public/img/bubblespeech.png" alt="">
-         </div>
+        <div class="position-relative  ms-5">
+          <img 
+            v-for="(item, i) in imageCompositionArray"
+            :key="item.id"
+            :class="[{'position-absolute': i != 0}, item.class]"
+            :src="item.src"     
+            :alt="item.alt"
+            >
+        </div>
       </div>
 
-      
+      <!-- cards -->
       <div class="_cards  _container_custom_sm  d-flex  justify-content-between">
-         <div class="_card">
-            <p><i class="fa-solid fa-globe  blue_"></i></p>
-            <h4 class="my-4">Worldwide Recognize</h4>
-            <p class="text_grey  w-100">Our online course certificates can be used around the world and also in most popular universities and companies</p>
-         </div>
-
-         <div class="_card">
-            <p><i class="fa-solid fa-desktop  blue_"></i></p>
-            <h4 class="my-4">Mostly Online Learning (sd)</h4>
-            <p class="text_grey">Masterstudy online certificates can be obtained in a range of specialized areas and typically take about a year to complete</p>
-         </div>
-
-         <div class="_card">
-            <p><i class="fa-solid fa-calendar-days  blue_"></i></p>
-            <h4 class="my-4">Graduate in as little as 1 Year</h4>
-            <p class="text_grey">Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy</p>
-         </div>
+        <div
+          v-for="item in cardsArray"
+          :key="item.id"
+          class="_card"
+          >
+          <p>
+            <i 
+              :class="item.class"
+              class="fa-solid  blue_">
+            </i>
+          </p>
+          <h4 class="my-4">{{ item.title }}</h4>
+          <p class="text_grey">{{ item.text }}</p>
+        </div>
       </div>
 
   </section>
@@ -92,7 +103,6 @@ export default {
     bottom: 73%;
   }
 }
-
 
 
 ._cards{
