30f8aab5eef9b61806bef5b1d5ae47a7250cf344
Jacopo Lombardi <38923043+JacopoLombardi@users.noreply.github.com>
2024-04-08 11:47:57 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+
+
+<!-- Javascript -->
+<script>
+
+
+export default {
+  data(){
+    return{
+
+    };
+  },
+
+  methods:{
+
+  },
+};
+</script>
+
+
+
+<!-- HTML -->
+<template>
+
+  
+</template>
+
+
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+@use '../assets/scss/partials/variables' as *;
+
+</style>
